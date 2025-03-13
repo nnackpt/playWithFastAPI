104b8dee6028941470621b598710ed5341efca5a
nnackpt <koonnack55@gmail.com>
2025-03-13 16:54:07 +0700

    Update DB

--- database.py
+++ database.py
@@ -9,9 +9,14 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String)
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
+    phone_number = Column(String)
     hashed_password = Column(String)
+    gender = Column(String)
 
 Base.metadata.create_all(bind=engine)
 

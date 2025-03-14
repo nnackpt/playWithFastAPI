9b37c724286dd4c1e40fca1a0ae30342d40bcca6
nnackpt <koonnack55@gmail.com>
2025-03-15 00:57:35 +0700

    Add scripts

---
+++ static/js/profile.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('logoutBtn').addEventListener('click', function() {
+        window.location.href = '/logout';
+    });
+}); 

84ceb35182a2c8cbbaa818ab55b86b9318665615
nnackpt <koonnack55@gmail.com>
2025-03-16 02:15:30 +0700

    Add tables

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
@@ -17,5 +17,19 @@ class User(Base):
     phone_number = Column(String)
     hashed_password = Column(String)
     gender = Column(String)
+    profile_image = Column(String, nullable=True)
+
+    preferences = relationship("UserPreferences", back_populates="user", uselist=False)
+
+class UserPreferences(Base):
+    __tablename__ = "user_preferences"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), unique=True)
+    email_notifications = Column(Boolean, default=True)
+    dark_mode = Column(Boolean, default=False)
+    language = Column(String, default="en")
+
+    user = relationship("User", back_populates="preferences")
 
 Base.metadata.create_all(bind=engine)
